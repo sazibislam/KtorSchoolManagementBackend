dfc474ae092224b396eae7ec2e03d881eb6975ca
sazib <sazib.islam@btracsl.com>
2022-09-26 13:58:40 +0600

    routing content type

--- src/main/kotlin/com/example/plugins/route/user/UserRoutes.kt
+++ src/main/kotlin/com/example/plugins/route/user/UserRoutes.kt
@@ -2,6 +2,7 @@ package com.example.plugins.route.user
 
 import com.example.data.repository.user.UserRepository
 import com.example.plugins.security.UserPrincipalForUser
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -9,28 +10,30 @@ import io.ktor.server.routing.*
 
 fun Application.userRoutes(repository: UserRepository) {
     routing {
-        authenticate {
-            route("/logout") {
-                post {
-                    val principal = call.principal<UserPrincipalForUser>()
-                    val result = repository.logoutUser(principal?.id!!)
-                    call.respond(result.statusCode, result)
+        accept(ContentType("application", "json")) {
+            authenticate {
+                route("/logout") {
+                    post {
+                        val principal = call.principal<UserPrincipalForUser>()
+                        val result = repository.logoutUser(principal?.id!!)
+                        call.respond(result.statusCode, result)
+                    }
                 }
-            }
 
-            route("/user") {
-                get {
-                    val principal = call.principal<UserPrincipalForUser>()
-                    val result = repository.getUser(principal?.id!!)
-                    call.respond(result.statusCode, result)
+                route("/user") {
+                    get {
+                        val principal = call.principal<UserPrincipalForUser>()
+                        val result = repository.getUser(principal?.id!!)
+                        call.respond(result.statusCode, result)
+                    }
                 }
-            }
 
-            route("/notification") {
-                get {
-                    val principal = call.principal<UserPrincipalForUser>()
-                    val result = repository.getNotification(principal?.id!!)
-                    call.respond(result.statusCode, result)
+                route("/notification") {
+                    get {
+                        val principal = call.principal<UserPrincipalForUser>()
+                        val result = repository.getNotification(principal?.id!!)
+                        call.respond(result.statusCode, result)
+                    }
                 }
             }
         }
