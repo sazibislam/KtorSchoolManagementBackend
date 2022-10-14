980b39d0a77ee4d855e4f2bf05a2e0a43a93f0d3
sazib <sazib.islam@btracsl.com>
2022-10-14 12:00:26 +0600

    blog table

--- src/main/kotlin/com/example/data/service/user/UserServiceImpl.kt
+++ src/main/kotlin/com/example/data/service/user/UserServiceImpl.kt
@@ -1,15 +1,20 @@
 package com.example.data.service.user
 
 import com.example.data.db.DatabaseFactory.dbQuery
+import com.example.data.db.extention.toComment
 import com.example.data.db.extention.toNotification
+import com.example.data.db.extention.toPost
 import com.example.data.db.extention.toUser
 import com.example.data.db.schema.NotificationTable
 import com.example.data.db.schema.UserTable
+import com.example.data.db.schema.blog.*
 import com.example.data.model.Notification
+import com.example.data.model.Post
 import com.example.data.model.User
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.statements.InsertStatement
 
 class UserServiceImpl : UserService {
     override suspend fun getUser(id: Int): User {
@@ -27,13 +32,57 @@ class UserServiceImpl : UserService {
         NotificationTable.deleteWhere { NotificationTable.userId eq id } > 0
     }
 
-    override suspend fun insertMocNotificationData(id: Int) {
+    override suspend fun getPosts(id: Int): List<Post> = dbQuery {
+        PostTable.select { PostTable.userId eq id }.mapNotNull { row_ ->
+            val data = row_.toPost()
+            if (data != null) {
+                data.comments = PostCommentTable.select { PostCommentTable.postId eq data.id }.mapNotNull { comment_ ->
+                    comment_.toComment()
+                }
+                data.reach = PostReachTable.select { PostReachTable.postId eq data.id }.map { reach_ ->
+                    reach_[PostReachTable.reach].toString()
+                }.firstOrNull()
+            }
+            data
+        }
+    }
+
+    override suspend fun insertMocData(id: Int) {
+
+        var statement: InsertStatement<Number>? = null
         dbQuery {
-            NotificationTable.insert {
+            statement = PostTable.insert {
                 it[userId] = id
-                it[title] = "Test Title"
-                it[note] = "Test Note"
+                it[title] = "Test Title $id"
+                it[published] = true
+                it[image] = ""
+                it[slug] = ""
             }
         }
+
+            dbQuery {
+                TagTable.insert {
+                    it[name] = "Test Tag post id"
+                }
+
+                PostTagTable.insert {
+                    it[postId] = 15
+                    it[tagId] = 1
+                }
+
+                PostDescriptionTable.insert {
+                    it[postId] = 15
+                    it[description] = "Test Description"
+                }
+                PostCommentTable.insert {
+                    it[postId] = 15
+                    it[comment] = "Test Comment"
+                }
+                PostReachTable.insert {
+                    it[postId] = 15
+                    it[reach] = 1
+                }
+            }
+
     }
 }
