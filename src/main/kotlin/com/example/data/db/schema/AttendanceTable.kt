3bb34d8806f99eb34635d49e5542437e1a81a335
sazib <sazib.islam@btracsl.com>
2022-10-20 13:07:04 +0600

    batch student

--- src/main/kotlin/com/example/data/db/schema/AttendanceTable.kt
+++ src/main/kotlin/com/example/data/db/schema/AttendanceTable.kt
@@ -4,15 +4,11 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
-/*
-* teacher table
-* */
 object AttendanceTable : Table("attendance") {
     val id = integer("id").autoIncrement()
-    val teacherId = integer("teacher_id").references(UserTable.id) //foreign key to teacher
+    val batchId = integer("batch_id").references(BatchTable.id)
+    val studentId = integer("student_id").references(StudentTable.id)
     val isPresent = bool("is_present").clientDefault { false }
-    val note = text("note")
-    val attendanceList = text("students_id_list")
     val createdAt = datetime("created_at").clientDefault { LocalDateTime.now() }
     val updatedAt = datetime("updated_at").clientDefault { LocalDateTime.now() }
 
