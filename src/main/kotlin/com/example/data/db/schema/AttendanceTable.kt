d03b1d0a2d444b1883c7173e8a8f605ba62ebc3d
sazib <sazib.islam@btracsl.com>
2022-10-20 13:32:21 +0600

    attendance

--- src/main/kotlin/com/example/data/db/schema/AttendanceTable.kt
+++ src/main/kotlin/com/example/data/db/schema/AttendanceTable.kt
@@ -9,6 +9,7 @@ object AttendanceTable : Table("attendance") {
     val batchId = integer("batch_id").references(BatchTable.id)
     val studentId = integer("student_id").references(StudentTable.id)
     val isPresent = bool("is_present").clientDefault { false }
+    val date = datetime("date").clientDefault { LocalDateTime.now() }
     val createdAt = datetime("created_at").clientDefault { LocalDateTime.now() }
     val updatedAt = datetime("updated_at").clientDefault { LocalDateTime.now() }
 
